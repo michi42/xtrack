de06d6e8c47c240dbdf5053b903e25949367dc1f
giadarol <giovanni.iadarola@cern.ch>
2024-11-10 10:27:15 +0100

    Status

--- examples/mad_writer/001a_madng_rdt.py
+++ examples/mad_writer/001a_madng_rdt.py
@@ -32,19 +32,26 @@ local mtbl = twiss {sequence=lhc, X0=X0, trkrdt=rdts, info=2, saverdt=true}
 + send_cmd)
 
 out = mng.recv()
+
+# Add to table
 assert len(out[0]) == len(tw) + 1
-dct = {k: v[:-1] for k, v in zip(colums, out)}
-dct['name'] = tw.name
-tng = xt.Table(dct)
+for ii, nn in enumerate(colums):
+    if nn in tw.keys():
+        nn = nn + '_ng'
+    tw[nn] = out[ii][:-1]
+
+# dct = {k: v[:-1] for k, v in zip(colums, out)}
+# dct['name'] = tw.name
+# tng = xt.Table(dct)
 
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.figure(1)
 
-plt.plot(tng.s, np.abs(tng.f4000), label='f4000')
-plt.plot(tng.s, np.abs(tng.f2020), label='f2020')
-plt.plot(tng.s, np.abs(tng.f1120), label='f1120')
-plt.plot(tng.s, np.abs(tng.f3100), label='f3100')
+plt.plot(tw.s, np.abs(tw.f4000), label='f4000')
+plt.plot(tw.s, np.abs(tw.f2020), label='f2020')
+plt.plot(tw.s, np.abs(tw.f1120), label='f1120')
+plt.plot(tw.s, np.abs(tw.f3100), label='f3100')
 plt.xlabel('s [m]')
 plt.ylabel(r'|f_{jklm}|')
 plt.legend()
